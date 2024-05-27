84ff0418eb0694177e9b026b33c0437ee9ea3286
Lucas <cheroyonatan2@gmail.com>
2024-05-27 17:07:25 -0300

    Responsive Design

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-
-const adviceButton = document.getElementById("btn-next");
+//Variables
+const adviceBtn = document.getElementById("btn-next");
 const adviceParagraph = document.getElementById("quote-text");
 const numberAdvice = document.getElementById("id-advice");
 
-adviceButton.addEventListener("click", () => {
+
+//Eventos
+
+adviceBtn.addEventListener("click", () => {
     fetch("https://api.adviceslip.com/advice")
         .then(response => response.json())
+
         .then(data => {
             adviceParagraph.textContent = `"${data.slip.advice}"`;
             numberAdvice.textContent = `${data.slip.id}`
-            
         })
+
         .catch(error => {
             console.error("Error fetching advice:", error);
             adviceParagraph.textContent = "Sorry, could not fetch advice at the moment.";
